5ab61229737aa95c627f9ca8093cd3623a458e9f
Shrishti-Singh-15 <shrishtisingh.vns2001@gmail.com>
2024-05-15 00:05:17 +0530

    Commit

---
+++ src/Stacks/UsingArrayList.java
@@ -0,0 +1,48 @@
+package Stacks;
+
+import java.util.ArrayList;
+
+public class UsingArrayList {
+    static class Stack{
+        static ArrayList<Integer> list = new ArrayList<>();
+        public static boolean isEmpty(){
+            return list.size() == 0;
+        }
+
+        //push
+        public static void push(int data){
+            list.add(data);
+        }
+
+        //pop
+        public static int pop(){
+            if(isEmpty()){
+                return -1;
+            }
+            int top = list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top;
+        }
+
+        public static int peek(){
+            if(isEmpty()){
+                return -1;
+            }
+            int top = list.get(list.size()-1);
+            return top;
+        }
+    }
+
+    public static void main(String agrs[]){
+        Stack stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+
+        while(!stack.isEmpty()){
+            System.out.println(stack.peek());
+            stack.pop();
+        }
+    }
+}
